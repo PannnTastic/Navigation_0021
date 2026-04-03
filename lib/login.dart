aab0ea9fe4a7ed3b8a4731cb5b4045ad45be84f5
PannnTastic <pannntastic@gmail.com>
2026-04-03 11:33:43 +0700

    add LoginPage widget with initial setup

---
+++ lib/login.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:state_management/home.dart';
+import 'package:state_management/mainlayout.dart';
+import 'package:state_management/register.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
