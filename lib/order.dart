9f021fec70b7663bb4d6dd4e718669ceda0bad44
PannnTastic <pannntastic@gmail.com>
2026-04-03 11:36:08 +0700

    add OrderPage widget with initial setup

---
+++ lib/order.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:state_management/detail_order.dart';
+import 'package:state_management/mainlayout.dart';
+
+class OrderPage extends StatefulWidget {
+  const OrderPage({super.key});
+
+  @override
+  State<OrderPage> createState() => _OrderPageState();
+}
+
