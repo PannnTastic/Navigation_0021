5046dc99b3d0e2527df595176aeb1350ea59a629
PannnTastic <pannntastic@gmail.com>
2026-04-03 11:31:42 +0700

    add RegisterPage widget with initial setup

---
+++ lib/register.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:state_management/login.dart';
+import 'package:state_management/mainlayout.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
